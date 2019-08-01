707d47890e69db38451aad7db797bd32ef593136
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-01 12:10:38 +0300

    Initial setup to connect the project from visual studio with the git repository

---
+++ JsonStringValidator.Tests/ProgramTests.cs
@@ -0,0 +1,88 @@
+using System;
+using Xunit;
+
+namespace JsonStringValidator.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void IsValidJsonStringWhenEmptyStringShouldReturnValid()
+        {
+            Assert.Equal("Valid", Program.IsValidJsonString("\"\""));
+        }
+
+        [Fact]
+        public void IsValidJsonStringWhenNullShouldReturnInvalid()
+        {
+            Assert.Equal("Invalid", Program.IsValidJsonString(null));
+        }
+
+        [Fact]
+        public void IsValidJsonStringWhenOnlySpaceShouldReturnValid()
+        {
+            Assert.Equal("Valid", Program.IsValidJsonString("\" \""));
+        }
+
+        [Fact]
+        public void IsValidJsonStringWhenOnlyOneDoubleQuoteShouldReturnInvalid()
+        {
+            Assert.Equal("Invalid", Program.IsValidJsonString("\""));
+        }
+
+        [Fact]
+        public void IsValidJsonStringWhenOnlyControlCharacterShouldReturnInvalid()
+        {
+            const int EscapeChar = 27;
+
+            Assert.Equal("Invalid", Program.IsValidJsonString("\"" + Convert.ToChar(EscapeChar) + "\""));
+        }
+
+        [Fact]
+        public void IsValidJsonStringWhenAllowedCharacterAndControlCharacterShouldReturnInvalid()
+        {
+            const int EscapeChar = 27;
+
+            Assert.Equal("Invalid", Program.IsValidJsonString("\"2" + Convert.ToChar(EscapeChar) + "\""));
+        }
+
+        [Fact]
+        public void IsValidJsonStringWhenAllowedCharacterAndBackslashShouldReturnInvalid()
+        {
+            const int Backslash = 92;
+
+            Assert.Equal("Invalid", Program.IsValidJsonString("\"2" + Convert.ToChar(Backslash) + "\""));
+        }
+
+        [Fact]
+        public void IsValidJsonStringWhenAllowedCharacterAndQuotationMarkShouldReturnInvalid()
+        {
+            const int QuotationMark = 34;
+
+            Assert.Equal("Invalid", Program.IsValidJsonString("\"2" + Convert.ToChar(QuotationMark) + "\""));
+        }
+
+        [Fact]
+        public void IsValidJsonStringWhenIsNotWrappedOnDoubleQuotesShouldReturnInvalid()
+        {
+            Assert.Equal("Invalid", Program.IsValidJsonString("unquoted text"));
+        }
+
+        [Fact]
+        public void IsQuotedWhenInputDataIsNotWrappedOnDoubleQuotesShouldReturnFalse()
+        {
+            Assert.False(Program.IsQuoted("unquoted text"));
+        }
+
+        [Fact]
+        public void IsQuotedWhenInputDataIsWrappedOnDoubleQuotesShouldReturnTrue()
+        {
+            Assert.True(Program.IsQuoted("\"quoted text\""));
+        }
+
+        [Fact]
+        public void IsQuotedWhenInputDataIsOnlyOneDoubleQuoteShouldReturnFalse()
+        {
+            Assert.False(Program.IsQuoted("\""));
+        }
+    }
+}
---
+++ JsonStringValidator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JsonStringValidator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(IsValidJsonString(Console.ReadLine()));
+            Console.Read();
+        }
+
+        public static string IsValidJsonString(string inputData)
+        {
+            const int ControlCharUpperLimit = 31;
+            const int QuotationMark = 34;
+            const int Backslash = 92;
+            const int MinimumQuotationMarks = 2;
+
+            if (inputData == null || !IsQuoted(inputData))
+            {
+                return "Invalid";
+            }
+
+            string unquotedInputData = inputData.Substring(1, inputData.Length - MinimumQuotationMarks);
+
+            for (int i = 0; i < unquotedInputData.Length; i++)
+            {
+                if (unquotedInputData[i] <= Convert.ToChar(ControlCharUpperLimit) || unquotedInputData[i] == Convert.ToChar(Backslash) || unquotedInputData[i] == Convert.ToChar(QuotationMark))
+                {
+                    return "Invalid";
+                }
+            }
+
+            return "Valid";
+        }
+
+        public static bool IsQuoted(string inputData)
+        {
+            const int QuotationMark = 34;
+            const int MinimumQuotationMarks = 2;
+
+            if (inputData == null)
+            {
+                return false;
+            }
+
+            if (inputData.Length < MinimumQuotationMarks)
+            {
+                return false;
+            }
+
+            return inputData[0] == Convert.ToChar(QuotationMark) && inputData[inputData.Length - 1] == Convert.ToChar(QuotationMark);
+        }
+    }
+}
